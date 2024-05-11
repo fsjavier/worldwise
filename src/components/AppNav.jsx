dc76f35bd10169aab6865ccc621f5ada73932487
fsjavier <javier.fersal@gmail.com>
2024-05-11 08:17:45 +0200

    Add pages structure and routing

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,11 @@
+import styles from "./PageNav.module.css";
+
+function AppNav() {
+  return (
+    <div>
+      <nav className={styles.nav}>App navigation</nav>
+    </div>
+  );
+}
+
+export default AppNav;
