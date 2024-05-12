7559d78938ffa3925cb0c54ad52cb4c08e4a2520
fsjavier <javier.fersal@gmail.com>
2024-05-12 08:13:08 +0200

    Add fetch request and list cities

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,31 @@
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  const countries = cities.reduce((acc, cur) => {
+    if (!acc.map((el) => el.country).includes(cur.country)) {
+      return [...acc, { country: cur.country, emoji: cur.emoji }];
+    } else {
+      return acc;
+    }
+  }, []);
+
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking a city on the map" />
+    );
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
